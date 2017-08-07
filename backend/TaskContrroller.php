e7d6b576522640cd74355f06c09ade15e7eb1389
TriumfM <triumf_96@hotmail.com>
2017-08-07 10:05:47 +0200

    TaskController init

---
+++ backend/TaskContrroller.php
@@ -0,0 +1,24 @@
+<?php
+
+function findAll (){
+
+	$tasks = "Get all Task";
+
+}
+
+function findById ($id){
+
+}
+
+function store (){
+
+}
+
+function update ($id){
+	
+}
+
+function delete ($id){
+	
+}
+?>
